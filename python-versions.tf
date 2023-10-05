0f3739017b19ef6925c996338ba531f0d1edc876
Ross McFarland <rwmcfa1@gmail.com>
2023-10-05 16:26:29 -0700

    Add python 3.12 to the CI version matrix

--- python-versions.tf
+++ python-versions.tf
@@ -1,6 +1,6 @@
 variable "python_versions_active" {
   type    = list(string)
-  default = ["3.8", "3.9", "3.10", "3.11"]
+  default = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 }
 variable "python_version_current" {
   type    = string
